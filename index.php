09f08ae966a428c3165627899d3b3ce6a60542db
Jose Gomez <j.gomez4@sapalomera.com>
2025-03-07 11:38:31 +0100

    Arreglar bug

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Login Form</title>
   <link rel="stylesheet" href="assets/css/style.css">
-  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <!-- Correccio de bugs!!!! [if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body>
   <section class="container">
